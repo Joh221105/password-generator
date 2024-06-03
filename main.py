a54057b12114f7b3494b6f95fd441da7fafb3f4a
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-06-03 15:40:15 -0400

    refactors generate_password function to generate a password based on user criteria and defined length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import string
 import random
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,26 +12,54 @@ numbers = list(string.digits)
 
 password = []
 
+
 def generate_elements(num, type_list):
     generated_element = random.choices(type_list, k=num)
     password.extend(generated_element)
 
-def generate_password():
-    
+def generate_password(length, bool_lower, bool_upper, bool_symbols, bool_numbers):
+
     password.clear()
-    generate_password(num_lower_case, lower_case)
-    generate_password(num_upper_case, upper_case)
-    generate_password(num_symbol, symbols)
-    generate_password(num_digits, numbers)
 
-random.shuffle(password)
+    # keeps track of the number of elements in the generated password 
+    count = 0
 
-print(''.join(password))
+    while(count < length):
+
+        if bool_lower:
+            number_of_lower_case = random.randint(0, length//4)
+            generate_elements(number_of_lower_case, lower_case)
+            count += number_of_lower_case
+
+        if bool_upper:
+            number_of_upper_case = random.randint(0, length//4)
+            generate_elements(number_of_upper_case, upper_case)
+            count += number_of_upper_case
+
+        if bool_symbols:
+            number_of_symbols = random.randint(0, length//4)
+            generate_elements(number_of_symbols, symbols)
+            count += number_of_symbols
+
+        if bool_numbers:
+            number_of_digits = random.randint(0, length//4)
+            generate_elements(number_of_digits, numbers)
+            count += number_of_digits
+
+    random.shuffle(password)
+    if len(password) > length:
+        del password[length:]
+
+    return ''.join(password)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def home(name=None):
+#     return render_template("index.html",name=name)
 
-if __name__ == '__main__':
-    app.run()
+# @app.route("/generate")
+# def generate_password():
+#     password = generate_password(length, bool_lower, bool_upper, bool_symbols, bool_numbers)
+
+# if __name__ == '__main__':
+#     app.run()
